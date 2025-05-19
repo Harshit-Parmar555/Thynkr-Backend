411f0e6712ee513087403c1cb6b4ef30a677761f
Harshit_Parmar <parmarharshit441@gmail.com>
2025-05-19 21:55:59 +0530

    fix: add missing leading slash in auth.route.js route path

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -7,7 +7,7 @@ import { protectedRoute } from "../middlewares/auth.js";
 const authRouter = express.Router();
 
 authRouter.post("/signup", signup);
-authRouter.get("logout", logout);
+authRouter.get("/logout", logout);
 authRouter.get("/check-auth", protectedRoute, checkAuth);
 
 // Exporting the user router
